1c2c4f9720e56b34ae89c26f2bfab46edfd84dd1
Alexis Purslane <alexispurslane@pm.me>
2024-12-14 16:42:33 -0500

    Dealing with warnings

--- prometheus-mode.el
+++ prometheus-mode.el
@@ -1,4 +1,4 @@
-;;; prometheus-mode.el --- A mode to add a text editing grammar to Emacs, built on top of god-mode -*- lexical-binding: t -*-
+;;; prometheus-mode.el --- A minor mode to add a text editing grammar -*- lexical-binding: t -*-
 
 ;; Author: Alexis Purslane <alexispurslane@pm.me>
 ;; URL: https://github.com/alexispurslane/prometheus-mode
@@ -14,7 +14,7 @@
 ;; editing grammar, that works with modifier keys or using modal
 ;; editing via god mode, and which preserves all the same
 ;; keybindings and mnemonics, and all the same concepts and
-;; behavior, as regular emacs text editing, while also giving you
+;; behavior, as regular Emacs text editing, while also giving you
 ;; the advantages of a full grammar for text editing.
 
 ;;; License:
@@ -40,24 +40,133 @@
 (require 'god-mode)
 
 (defvar prometheus--last-point-position nil)
+(make-variable-buffer-local 'prometheus--last-point-position)
 (defvar prometheus--intentional-region-active nil)
 (defvar prometheus--god-command-in-progress nil)
 (defgroup prometheus nil
-    "Customization group for `prometheus-mode'"
+    "Customization group for `prometheus-mode'."
     :group 'editing
     :group 'god)
 (defcustom prometheus-excluded-major-modes '(magit-mode dired-mode gnus-group-mode gnus-summary-mode)
-    "The list of modes that auto-selection will not occur in, because
-it doesn't make sense."
+    "The list of modes that auto-selection will not occur in."
     :type 'listp
     :group 'prometheus)
 (defcustom prometheus-auto-timer-time 0.6
-    "The amount of idle time to wait until doing something, across
-several `prometheus-mode' features, including
-`isearch-forward-auto-timer' and backward timer,
-auto-deselection, etc."
+    "The amount of idle time to wait until doing something.
+Influences several things, including several `prometheus-mode'
+features, including `isearch-forward-auto-timer' and backward
+timer, auto-deselection, etc."
     :type 'numberp
     :group 'prometheus)
+
+(defun prometheus-god-mode-update-cursor-type ()
+    (setq cursor-type
+          (cond
+           ((or god-local-mode buffer-read-only) 'box)
+           (overwrite-mode 'hollow)
+           (t 'bar))))
+
+(defun prometheus-god-mode-toggle-on-overwrite ()
+    "Toggle god-mode on overwrite-mode."
+    (if (bound-and-true-p overwrite-mode)
+            (god-local-mode-pause)
+        (god-local-mode-resume)))
+
+;; if we pause in adding to our search term for more than
+;; half a second, assume we're done searching and want to
+;; now proceed through the search results; we can always
+;; hit i again to change it
+(defun prometheus-isearch-forward-auto-timer ()
+    (interactive)
+    (run-with-idle-timer prometheus-auto-timer-time nil
+                         (lambda ()
+                             (message "Search finished, activating god search mode")
+                             (god-mode-isearch-activate)))
+    (isearch-forward))
+(defun prometheus-isearch-backward-auto-timer ()
+    (interactive)
+    (run-with-idle-timer prometheus-auto-timer-time nil
+                         (lambda ()
+                             (message "Search finished, activating god search mode")
+                             (god-mode-isearch-activate)))
+    (isearch-backward))
+
+(defun prometheus--deselect ()
+    ;; if after the designated time has elapsed, the user
+    ;; hasn't run any commands, and doesn't seem to be in
+    ;; the process of running any commands, in what was
+    ;; just selected, automatically deselect it; it's
+    ;; likely that was just a trailing selection as a
+    ;; result of moving through the buffer!
+    (when (and god-local-mode
+               ;; if the minibuffer is open and focused,
+               ;; then the user is probably in the middle
+               ;; of running a command by name on the
+               ;; current selection, so don't deselect
+               (not (minibuffer-window-active-p (selected-window)))
+               ;; if the which key buffer is open the user
+               ;; is probably trying to figure out what to
+               ;; do/enter, so don't deselect what they
+               ;; selected yet
+               (not (and (boundp 'which-key--buffer)
+                         (get-buffer-window which-key--buffer)))
+               ;; if god-mode-self-insert is running right
+               ;; now, that means the user is in the middle
+               ;; of entering a god mode command on this
+               ;; region; don't deselect
+               (not (and (bound-and-true-p god-local-mode)
+                         (eq this-command 'god-mode-self-insert)))
+               ;; copied from which-key's code to detect
+               ;; the current prefix key if the user has
+               ;; entered one or more prefix keys they're
+               ;; in the middle of executing a non-god-mode
+               ;; command on the region, also don't
+               ;; deselect
+               (let ((this-command-keys (this-single-command-keys)))
+                   (when (and (vectorp this-command-keys)
+                              (> (length this-command-keys) 0)
+                              (eq (aref this-command-keys 0) 'key-chord)
+                              (bound-and-true-p key-chord-mode))
+                       (setq this-command-keys (this-single-command-raw-keys)))
+                   (length= this-command-keys 0)))
+        ;; deselect the current region and update the internal selection variables
+        (pop-mark)
+        (setq-local prometheus--last-point-position (point))))
+
+(defun prometheus--motion-selection ()
+    (cond
+     ((eq this-command 'set-mark-command)
+      (setq prometheus--intentional-region-active t))
+     ((and prometheus--last-point-position
+           (not prometheus--intentional-region-active)
+           (not (memq this-command '(forward-char
+                                     backward-char
+                                     self-insert-command
+                                     eat-self-input
+                                     next-line
+                                     previous-line
+                                     pixel-scroll-precision
+                                     pop-global-mark
+                                     expreg-expand
+                                     expreg-contract
+                                     undo)))
+           (not (memq major-mode prometheus-excluded-major-modes))
+           (not (and (boundp 'rectangle-mark-mode) rectangle-mark-mode))
+           (not isearch-mode))
+      (let ((inhibit-message t))
+          (run-with-idle-timer
+           (* 1.5 prometheus-auto-timer-time) nil
+           #'prometheus--deselect)
+          ;; we set two marks here so that if there are
+          ;; commands that take multiple regions, this will
+          ;; count as the end of the previous region and the
+          ;; beginning of the next, so that the text objects
+          ;; we proceed over linearly can act as the
+          ;; arguments to a command automatically.
+          (push-mark prometheus--last-point-position)
+          (push-mark prometheus--last-point-position nil t))))
+    (setq-local prometheus--last-point-position (point)))
+
 ;;;###autoload
 (define-minor-mode prometheus-mode
     "A modal editing minor mode for Emacs built on top of `god-mode'
@@ -75,104 +184,18 @@ all."
     ;; Enable god mode globally
     (god-mode)
     
-    ;; We want an in-line visual indication of whether we're
-    ;; in god mode or not
-    (defun prometheus-god-mode-update-cursor-type ()
-        (setq cursor-type
-              (cond
-               ((or god-local-mode buffer-read-only) 'box)
-               (overwrite-mode 'hollow)
-               (t 'bar))))
-
+    ;; We want an in-line visual indication of whether we're in god
+    ;; mode or not
     (add-hook 'post-command-hook #'prometheus-god-mode-update-cursor-type)
     
     ;; Kakoune-style prometheus--motion-selection
-    (make-variable-buffer-local 'prometheus--last-point-position)
     (add-hook 'deactivate-mark-hook (lambda ()
                                         (setq prometheus--intentional-region-active nil)))
-    (defun prometheus--deselect ()
-        ;; if after the designated time has elapsed, the user
-        ;; hasn't run any commands, and doesn't seem to be in
-        ;; the process of running any commands, in what was
-        ;; just selected, automatically deselect it; it's
-        ;; likely that was just a trailing selection as a
-        ;; result of moving through the buffer!
-        (when (and god-local-mode
-                   ;; if the minibuffer is open and focused,
-                   ;; then the user is probably in the middle
-                   ;; of running a command by name on the
-                   ;; current selection, so don't deselect
-                   (not (minibuffer-window-active-p (selected-window)))
-                   ;; if the which key buffer is open the user
-                   ;; is probably trying to figure out what to
-                   ;; do/enter, so don't deselect what they
-                   ;; selected yet
-                   (not (and (boundp 'which-key--buffer)
-                             (get-buffer-window which-key--buffer)))
-                   ;; if god-mode-self-insert is running right
-                   ;; now, that means the user is in the middle
-                   ;; of entering a god mode command on this
-                   ;; region; don't deselect
-                   (not (and (bound-and-true-p god-local-mode)
-                             (eq this-command 'god-mode-self-insert)))
-                   ;; copied from which-key's code to detect
-                   ;; the current prefix key if the user has
-                   ;; entered one or more prefix keys they're
-                   ;; in the middle of executing a non-god-mode
-                   ;; command on the region, also don't
-                   ;; deselect
-                   (let ((this-command-keys (this-single-command-keys)))
-                       (when (and (vectorp this-command-keys)
-                                  (> (length this-command-keys) 0)
-                                  (eq (aref this-command-keys 0) 'key-chord)
-                                  (bound-and-true-p key-chord-mode))
-                           (setq this-command-keys (this-single-command-raw-keys)))
-                       (length= this-command-keys 0)))
-            ;; deselect the current region and update the internal selection variables
-            (pop-mark)
-            (setq-local prometheus--last-point-position (point))))
-    (defun prometheus--motion-selection ()
-        (cond
-         ((eq this-command 'set-mark-command)
-          (setq prometheus--intentional-region-active t))
-         ((and prometheus--last-point-position
-               (not prometheus--intentional-region-active)
-               (not (memq this-command '(forward-char
-                                         backward-char
-                                         self-insert-command
-                                         eat-self-input
-                                         next-line
-                                         previous-line
-                                         pixel-scroll-precision
-                                         pop-global-mark
-                                         expreg-expand
-                                         expreg-contract
-                                         undo)))
-               (not (memq major-mode prometheus-excluded-major-modes))
-               (not (and (boundp 'rectangle-mark-mode) rectangle-mark-mode))
-               (not isearch-mode))
-          (let ((inhibit-message t))
-              (run-with-idle-timer
-               (* 1.5 prometheus-auto-timer-time) nil
-               #'prometheus--deselect)
-              ;; we set two marks here so that if there are
-              ;; commands that take multiple regions, this will
-              ;; count as the end of the previous region and the
-              ;; beginning of the next, so that the text objects
-              ;; we proceed over linearly can act as the
-              ;; arguments to a command automatically.
-              (push-mark prometheus--last-point-position)
-              (push-mark prometheus--last-point-position nil t))))
-        (setq-local prometheus--last-point-position (point)))
+    
     (add-hook 'post-command-hook 'prometheus--motion-selection)
+
     ;; When we're overwriting text, we want to actually be
     ;; able to edit text automatically without interference
-    (defun prometheus-god-mode-toggle-on-overwrite ()
-        "Toggle god-mode on overwrite-mode."
-        (if (bound-and-true-p overwrite-mode)
-                (god-local-mode-pause)
-            (god-local-mode-resume)))
-
     (add-hook 'overwrite-mode-hook #'prometheus-god-mode-toggle-on-overwrite)
     
     ;; Extend quake's version of `escape-dwim' to cancel god
@@ -220,24 +243,7 @@ current buffer in God Mode."
     (require 'god-mode-isearch)
     (define-key god-mode-isearch-map (kbd "i") #'god-mode-isearch-disable)
     (define-key isearch-mode-map (kbd "<escape>") #'god-mode-isearch-activate)
-    ;; if we pause in adding to our search term for more than
-    ;; half a second, assume we're done searching and want to
-    ;; now proceed through the search results; we can always
-    ;; hit i again to change it
-    (defun prometheus-isearch-forward-auto-timer ()
-        (interactive)
-        (run-with-idle-timer prometheus-auto-timer-time nil
-                             (lambda ()
-                                 (message "Search finished, activating god search mode")
-                                 (god-mode-isearch-activate)))
-        (isearch-forward))
-    (defun prometheus-isearch-backward-auto-timer ()
-        (interactive)
-        (run-with-idle-timer prometheus-auto-timer-time nil
-                             (lambda ()
-                                 (message "Search finished, activating god search mode")
-                                 (god-mode-isearch-activate)))
-        (isearch-backward))
+    
     ;; next we ensure that we can get out of isearch the way we get out of everything else
     (define-key god-mode-isearch-map (kbd "<escape>") 'prometheus--escape-dwim)
     ;; next, we extend the flexibility of traditional
